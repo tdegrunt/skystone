f6a351bb0886e3e6080f2f8a5883b893cabb5f39
Andre Meij <andre@socialreferral.com>
2012-10-02 23:45:39 +0200

    Command is a reserved name by purogin?

--- sky_stone_plugin.rb
+++ sky_stone_plugin.rb
@@ -14,22 +14,24 @@ class SkyStonePlugin
     @plugin.broadcast "Loaded 'SkyStone' plugin"
 
     public_command('skystone', 'Skystone', '/skystone ...') do |me, *args|
-      command(me, args)
+      player_command(me, args)
     end
 
     @storage_cart_system = SkyStone::StorageCartSystem.new(self)
     @cart_routes = SkyStone::CartRoutes.new(self)
   end
 
-  def command(player, arguments)
+  def player_command(player, arguments)
 
-    subcommand = arguments.shift
+    if arguments.length > 0
+      subcommand = arguments.shift
 
-    case subcommand.to_sym
-    when :route
-      @cart_routes.command(player, arguments)
-    when :storage
-      @storage_cart_system.command(player, arguments)
+      case subcommand.to_sym
+      when :route
+        @cart_routes.player_command(player, arguments)
+      when :storage
+        @storage_cart_system.player_command(player, arguments)
+      end
     end
   end
 
